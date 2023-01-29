c0d601d6cb0a2908480f7d34ff1e9565e16e8fd0
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-01-29 16:27:38 +0300

    dz3_z19

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int == Convert.ToInt32(Console.ReadLine());
+
+if ((n/10000) == (n %10)) n = (n/10) % 1000;
+if ((n/100) == (n %10)) Console.WriteLine("Полиндром");
+else System.Console.WriteLine("Не полиндром");
