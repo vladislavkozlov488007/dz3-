448b22f8e8d3407f312fae9f37bbb58a92d1548d
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-01-29 16:28:06 +0300

    dz3_z21

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿Console.WriteLine("Введите пятизначное число");
-int == Convert.ToInt32(Console.ReadLine());
+﻿System.Console.WriteLine("Введите координаты первой точки");
+int x1 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
+int x3 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите координаты второй точки");
+int y1 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int y3 = int.Parse(Console.ReadLine());
 
-if ((n/10000) == (n %10)) n = (n/10) % 1000;
-if ((n/100) == (n %10)) Console.WriteLine("Полиндром");
-else System.Console.WriteLine("Не полиндром");
+double result = Math.Round(Math.Sqrt(Math.Pow((x1-y1),2) + Math.Pow((x2-y2),2) + Math.Pow((x3-y3),2)),2);
+System.Console.WriteLine($"Расстояние между первой и второй точкой равно {result}");
